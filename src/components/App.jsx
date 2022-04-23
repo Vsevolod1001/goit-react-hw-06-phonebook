529472232032af3d225e1e3c0ef6ad124060e6fb
Vsevolod1001 <sevant.101@gmail.com>
2022-04-23 21:08:05 +0300

    +proptypes, без persist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import { AddContact } from './AddContact/AddContact';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
+import { Section } from './Section/Section';
 
 export const App = () => {
   return (
     <div style={{ width: 400 }}>
+      <h1>Phonebook</h1>
       <AddContact />
-
-      <Filter />
-
-      <ContactList />
+      <Section name="Contacts">
+        <Filter />
+        <ContactList />
+      </Section>
     </div>
   );
 };
